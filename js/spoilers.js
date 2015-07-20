6a4eb3821bb4742befa5bc3420c713c4400c29cb
Tim Aldridge <taldridge@curse.com>
2015-07-20 16:40:49 -0500

    Fixed JS that was causing autohiding on page load.

--- js/spoilers.js
+++ js/spoilers.js
@@ -1,9 +1,17 @@
-$( '.spoilers-button' ).toggle( function() {
-	$( this ).parents( '.spoilers' ).find( '.spoilers-body' ).show();
-	$( this ).children( '.spoilers-show' ).hide();
-	$( this ).children( '.spoilers-hide' ).show();
-}, function() {
-	$( this ).parents( '.spoilers' ).find( '.spoilers-body' ).hide();
-	$( this ).children( '.spoilers-show' ).show();
-	$( this ).children( '.spoilers-hide' ).hide();
-});
+function Spoilers($) {
+	'use strict';
+
+	this.init = function() {
+		$('.spoilers-button').on('click', spoiler.reveal);
+	};
+
+	var spoiler = {
+		reveal: function() {
+			$(this).parents('.spoilers').find('.spoilers-body').toggle();
+			$(this).children().toggle();
+		}
+	}
+}
+
+var SE = new Spoilers(jQuery);
+jQuery(document).ready(SE.init);
