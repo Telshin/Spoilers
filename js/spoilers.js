b0f67bc20b8c362cd8614c9d31a75e018753f4a7
Kris <cblair91@gmail.com>
2016-05-03 00:36:19 +0100

    Cleanup to MW standards [3/3] - JS

--- js/spoilers.js
+++ js/spoilers.js
@@ -3,8 +3,11 @@
 
 	function init() {
 		$('.spoilers-button').click(function() {
-			$(this).parents('.spoilers').children('.spoilers-body').toggle();
-			$(this).children().toggle();
+			var $parent = $(this).parent(),
+				showMsg = $parent.attr('data-showtext') || msg.msg('spoilers_show_default'),
+				hideMsg = $parent.attr('data-hidetext') || msg.msg('spoilers_hide_default');
+			$parent.children('.spoilers-button').text($parent.children('.spolers-body:visible').length ? hideMsg : showMsg);
+			$parent.children('.spoilers-body').slideToggle();
 		});
 	}
 
