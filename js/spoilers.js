9d9f31fd43da2954c72645bc84b2c68294fc897c
Kris <cblair91@gmail.com>
2016-05-03 00:37:13 +0100

    Spelling mistake

--- js/spoilers.js
+++ js/spoilers.js
@@ -4,8 +4,8 @@
 	function init() {
 		$('.spoilers-button').click(function() {
 			var $parent = $(this).parent(),
-				showMsg = $parent.attr('data-showtext') || msg.msg('spoilers_show_default'),
-				hideMsg = $parent.attr('data-hidetext') || msg.msg('spoilers_hide_default');
+				showMsg = $parent.attr('data-showtext') || mw.msg('spoilers_show_default'),
+				hideMsg = $parent.attr('data-hidetext') || mw.msg('spoilers_hide_default');
 			$parent.children('.spoilers-button').text($parent.children('.spolers-body:visible').length ? hideMsg : showMsg);
 			$parent.children('.spoilers-body').slideToggle();
 		});
