b299b77c71018398f93618908ed8dbcd769f81c8
Kris <cblair91@gmail.com>
2016-07-27 05:54:58 +0100

    Changes for PR #24
    Clean up the code into better formatting
    Make the default text based on if it's hidden/shown by default.

--- js/spoilers.js
+++ js/spoilers.js
@@ -8,12 +8,12 @@
 				showMsg = $parent.attr('data-showtext') || mw.msg('spoilers_show_default'),
 				hideMsg = $parent.attr('data-hidetext') || mw.msg('spoilers_hide_default');
 			$this
-				.text($parent.children('.spolers-body:visible').length ? showMsg : hideMsg);
+				.text($parent.children('.spolers-body:visible').length ? showMsg : hideMsg)
 				.click(function() {
-					var $parent = $(this).parent(),
 					$parent.children('.spoilers-button').text($parent.children('.spolers-body:visible').length ? hideMsg : showMsg);
 					$parent.children('.spoilers-body').slideToggle();
 				});
+		});
 	}
 
 	$(init);
