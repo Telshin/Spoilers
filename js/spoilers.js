9ceb93f12079249b3108d54f23c43338a5e22462
Kris <cblair91@gmail.com>
2016-07-27 05:51:54 +0100

    Changes for PR

--- js/spoilers.js
+++ js/spoilers.js
@@ -2,13 +2,18 @@
 	'use strict';
 
 	function init() {
-		$('.spoilers-button').click(function() {
-			var $parent = $(this).parent(),
+		$('.spoilers-button').each(function() {
+			var $this = $(this),
+				$parent = $this.parent(),
 				showMsg = $parent.attr('data-showtext') || mw.msg('spoilers_show_default'),
 				hideMsg = $parent.attr('data-hidetext') || mw.msg('spoilers_hide_default');
-			$parent.children('.spoilers-button').text($parent.children('.spolers-body:visible').length ? hideMsg : showMsg);
-			$parent.children('.spoilers-body').slideToggle();
-		});
+			$this
+				.text($parent.children('.spolers-body:visible').length ? showMsg : hideMsg);
+				.click(function() {
+					var $parent = $(this).parent(),
+					$parent.children('.spoilers-button').text($parent.children('.spolers-body:visible').length ? hideMsg : showMsg);
+					$parent.children('.spoilers-body').slideToggle();
+				});
 	}
 
 	$(init);
