3b6bd3d6c1ed6640b0837577ac40846c35d8e7f7
Telshin <timmrysk@gmail.com>
2017-04-03 08:28:32 -0700

    Update spoilers.js
    Fixed typo

--- js/spoilers.js
+++ js/spoilers.js
@@ -8,7 +8,7 @@
 				showMsg = $parent.attr('data-showtext') || mw.msg('spoilers_show_default'),
 				hideMsg = $parent.attr('data-hidetext') || mw.msg('spoilers_hide_default');
 			$this
-				.text($parent.children('.spolers-body:visible').length ? showMsg : hideMsg)
+				.text($parent.children('.spoilers-body:visible').length ? showMsg : hideMsg)
 				.click(function() {
 					$parent.children('.spoilers-button').text($parent.children('.spolers-body:visible').length ? hideMsg : showMsg);
 					$parent.children('.spoilers-body').slideToggle();
