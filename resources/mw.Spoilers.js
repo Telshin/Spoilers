859ee145cafa998b7547dee5de87c3e1a6c208c6
Kris <cblair91@gmail.com>
2017-10-13 18:41:59 +0100

    Set shown state properly

--- resources/mw.Spoilers.js
+++ resources/mw.Spoilers.js
@@ -5,14 +5,14 @@
 		var $spoilerButton = $( '.spoilers-button' );
 		$spoilerButton.each( function() {
 			var $parent = $( this ).parent();
-			$parent.children( '.spoilers-button' ).text( $parent.attr( 'data-showtext' ) || mw.msg( 'spoilers_show_default' ) );
+			$parent.children( '.spoilers-button' ).text( $parent.data( 'showtext' ) || mw.msg( 'spoilers_show_default' ) );
 		});
 		$spoilerButton.click( function() {
 			var $parent = $( this ).parent(),
 				shown = $parent.data( 'shown' ),
-				showMsg = $parent.attr( 'data-showtext' ) || mw.msg( 'spoilers_show_default' ),
-				hideMsg = $parent.attr( 'data-hidetext' ) || mw.msg( 'spoilers_hide_default' );
-			$parent.attr('data-shown', !shown)
+				showMsg = $parent.data( 'showtext' ) || mw.msg( 'spoilers_show_default' ),
+				hideMsg = $parent.data( 'hidetext' ) || mw.msg( 'spoilers_hide_default' );
+			$parent.data('shown', !shown);
 			$parent.children( '.spoilers-button' ).text( shown ? hideMsg : showMsg );
 			$parent.children( '.spoilers-body' ).slideToggle();
 		});
