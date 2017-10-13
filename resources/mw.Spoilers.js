b9ab03a4d316d0aaaf3e7cdffc2befb6c283a506
Kris <cblair91@gmail.com>
2017-10-13 18:39:22 +0100

    Apply data-shown to the correct elem

--- resources/mw.Spoilers.js
+++ resources/mw.Spoilers.js
@@ -5,15 +5,16 @@
 		var $spoilerButton = $( '.spoilers-button' );
 		$spoilerButton.each( function() {
 			var $parent = $( this ).parent();
-			$parent.children( '.spoilers-button' ).text( $parent.attr( 'data-hidetext' ) || mw.msg( 'spoilers_hide_default' ) );
+			$parent.children( '.spoilers-button' ).text( $parent.attr( 'data-showtext' ) || mw.msg( 'spoilers_show_default' ) );
 		});
 		$spoilerButton.click( function() {
 			var $parent = $( this ).parent(),
 				shown = $parent.attr( 'data-shown' ),
 				showMsg = $parent.attr( 'data-showtext' ) || mw.msg( 'spoilers_show_default' ),
 				hideMsg = $parent.attr( 'data-hidetext' ) || mw.msg( 'spoilers_hide_default' );
+			$parent.attr('data-shown', !shown)
 			$parent.children( '.spoilers-button' ).text( shown ? hideMsg : showMsg );
-			$parent.children( '.spoilers-body' ).attr('data-shown', !shown).slideToggle();
+			$parent.children( '.spoilers-body' ).slideToggle();
 		});
 	};
 
