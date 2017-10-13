cbb24cfec59b3b09a61b4e0c0ee1b8e751baddba
Kris <cblair91@gmail.com>
2017-10-13 18:30:56 +0100

    Derp

--- Spoilers.hooks.php
+++ Spoilers.hooks.php
@@ -32,11 +32,13 @@ class Spoilers {
 	 */
 	static public function spoilerMagicWord( Parser &$parser, PPFrame $frame, array $args ) {
 		$params = self::extractOptions( $args, $frame );
-		print_r($params);
 		$parser->getOutput()->addModules( 'ext.spoilers' );
 		$showText	=	isset( $params['show'] ) ? " data-showtext='" . htmlentities( $params['show'], ENT_QUOTES ) . "'" : "";
 		$hideText	=	isset( $params['hide'] ) ? " data-hidetext='" . htmlentities( $params['hide'], ENT_QUOTES ) . "'" : "";
-		$output		=	"<div class='spoilers'{$showText}{$hideText}<span class='spoilers-button'></span><div class='spoilers-body'>{$params['1']}</div></div>";
+		$output		=	"<div class='spoilers'{$showText}{$hideText}>
+	<span class='spoilers-button'></span>
+	<div class='spoilers-body'>{$params['1']}</div>
+</div>";
 		return [
 			'text'		=> $output,
 			'noparse'	=> true,
@@ -58,7 +60,6 @@ class Spoilers {
 				$results['1'] = $value;
 			}
 		}
-		print_r($results);
 		return $results;
 	}
 }
