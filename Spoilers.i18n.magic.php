710e8c0f8b3a4dd91bc43753b39dee20ffdb654b
Kris <cblair91@gmail.com>
2017-10-13 17:47:03 +0100

    Okay, lets try loading a magic i18n

---
+++ Spoilers.i18n.magic.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Spoilers
+ * Spoilers i18n Magic
+ *
+ * @author: Telshin, Developaws
+ * @license: LGPL-3.0 http://opensource.org/licenses/lgpl-3.0.html
+ * @package: Spoilers
+ * @link: http://www.mediawiki.org/wiki/Extension:Spoilers
+ */
+$magicWords = [];
+
+$magicWords['en'] = [
+	'spoiler' => [ 0, 'spoiler' ]
+];
