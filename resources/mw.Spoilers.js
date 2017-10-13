2cf3f17b2b6f103081f6fe01c16064009c25968d
Kris <cblair91@gmail.com>
2017-10-13 18:38:12 +0100

    Default state

--- resources/mw.Spoilers.js
+++ resources/mw.Spoilers.js
@@ -2,7 +2,12 @@
 	'use strict';
 
 	mw.Spoilers = function ( ) {
-		$( '.spoilers-button' ).click( function() {
+		var $spoilerButton = $( '.spoilers-button' );
+		$spoilerButton.each( function() {
+			var $parent = $( this ).parent();
+			$parent.children( '.spoilers-button' ).text( $parent.attr( 'data-hidetext' ) || mw.msg( 'spoilers_hide_default' ) );
+		});
+		$spoilerButton.click( function() {
 			var $parent = $( this ).parent(),
 				shown = $parent.attr( 'data-shown' ),
 				showMsg = $parent.attr( 'data-showtext' ) || mw.msg( 'spoilers_show_default' ),
