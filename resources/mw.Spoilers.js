31f2efc432faa8a0fd64db8b75150a603595fe67
Kris <cblair91@gmail.com>
2018-09-28 18:27:18 +0100

    Actually push the JS update

--- resources/mw.Spoilers.js
+++ resources/mw.Spoilers.js
@@ -1,14 +1,14 @@
-( function( mw, $ ) {
+( function ( mw, $ ) {
 	'use strict';
 
 	mw.Spoilers = function () {
 		var $spoilerButton = $( '.spoilers-button' );
-		$spoilerButton.each( function() {
+		$spoilerButton.each( function () {
 			var $parent = $( this ).parent();
 			$parent.children( '.spoilers-button' ).text( $parent.data( 'showtext' ) || mw.msg( 'spoilers_show_default' ) );
 			$parent.data( 'shown', false );
 		} );
-		$spoilerButton.click( function() {
+		$spoilerButton.click( function () {
 			var $parent = $( this ).parent(),
 				shown = $parent.data( 'shown' ),
 				showMsg = $parent.data( 'showtext' ) || mw.msg( 'spoilers_show_default' ),
@@ -22,4 +22,4 @@
 	$( function () {
 		mw.Spoilers();
 	} );
-} ( mediaWiki, jQuery ) );
+}( mediaWiki, jQuery ) );
